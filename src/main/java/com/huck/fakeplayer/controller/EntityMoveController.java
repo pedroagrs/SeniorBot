b159aed9482d3e2afbd297d7c1a23bf776529af5
Pedro Aguiar <43187922+PedroHAguiar@users.noreply.github.com>
2020-09-26 08:13:53 -0300

    fix bugs all versions

--- src/main/java/com/huck/fakeplayer/controller/EntityMoveController.java
+++ src/main/java/com/huck/fakeplayer/controller/EntityMoveController.java
@@ -4,9 +4,15 @@ import com.huck.fakeplayer.main.FakePlayerPlugin;
 import com.huck.fakeplayer.utils.NmsUtils;
 import lombok.Getter;
 import lombok.SneakyThrows;
+import net.minecraft.server.v1_15_R1.PacketPlayInEntityAction;
+import net.minecraft.server.v1_15_R1.PacketPlayOutAnimation;
+import net.minecraft.server.v1_15_R1.Entity;
+
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @Getter
 public class EntityMoveController extends BukkitRunnable {
@@ -15,18 +21,22 @@ public class EntityMoveController extends BukkitRunnable {
     private int step;
 
     private final EntityJumpController jumpController;
+    private final boolean isLastVersions;
 
-    private final String version = NmsUtils.getServerVersion();
+    private final String version;
 
-    public EntityMoveController(Object player) {
+    public EntityMoveController(Object player, String version) {
         this.player = player;
         this.step = 0;
-        this.jumpController = new EntityJumpController(player);
+        this.version = version;
+        this.isLastVersions = version.startsWith("v1_14") || version.startsWith("v1_15") || version.startsWith("v1_16");
+
+        this.jumpController = new EntityJumpController(player, version);
 
         runTaskTimer(FakePlayerPlugin.getPlugin(), 4L, 20L);
     }
 
-    public void nextStep() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void nextStep() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
 
         if (jumpController.isJumping()) return;
 
@@ -38,6 +48,10 @@ public class EntityMoveController extends BukkitRunnable {
             case 3:
             case 5:
                 player.getClass().getMethod("setSneaking", boolean.class).invoke(player, true);
+
+                if (isLastVersions)
+                    setSneakingLastVersions(true);
+
                 break;
             case 6:
                 if (version.startsWith("v1_8")) // MAIN_HAND
@@ -47,12 +61,18 @@ public class EntityMoveController extends BukkitRunnable {
 
                     final Object mainHand = enumHandClass.getEnumConstants()[0];
 
-                    player.getClass().getMethod("a", enumHandClass).invoke(player, mainHand);
+                    if (version.startsWith("v1_16"))
+                        player.getClass().getMethod("swingHand", enumHandClass).invoke(player, mainHand);
+                    else
+                        player.getClass().getMethod("a", enumHandClass).invoke(player, mainHand);
                 }
                 break;
             case 4:
             case 7:
                 player.getClass().getMethod("setSneaking", boolean.class).invoke(player, false);
+
+                if (isLastVersions)
+                    setSneakingLastVersions(false);
                 break;
         }
 
@@ -68,5 +88,25 @@ public class EntityMoveController extends BukkitRunnable {
     @Override
     public void run() {
         nextStep();
+    }
+
+    private final Class<?> entityHumanClass = NmsUtils.getNMSClass("Entity");
+    private final Class<?> entityPoseClass = NmsUtils.getNMSClass("EntityPose");
+
+    protected void setSneakingLastVersions(boolean flag) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = null;
+
+        int toggle = 0;
+
+        if (flag) toggle = 5;
+
+        if (version.startsWith("v1_14"))
+            method = entityHumanClass.getDeclaredMethod("b", entityPoseClass);
+
+        else method = entityHumanClass.getDeclaredMethod("setPose", entityPoseClass);
+
+        method.setAccessible(true);
+
+        method.invoke(player, entityPoseClass.getEnumConstants()[toggle]);
     }
 }
